7ebb78e03a65739657567f68d3b24c11a49631a0
Gustavo Alda <102753327+alda510@users.noreply.github.com>
2022-10-17 11:16:43 -0300

    Add files via upload

--- desafio2-js/js/app.js
+++ desafio2-js/js/app.js
@@ -1,136 +1,136 @@
 // import { validacoes } from "./validacoes";
 // window.onload = function () {
-    const usernameEl = document.querySelector('#username');
-    const passwordEl = document.querySelector('#password');
+//     const usernameEl = document.querySelector('#username');
+//     const passwordEl = document.querySelector('#password');
 
-    const form = document.querySelector('#signup');
-
-
-    const checkUsername = () => {
-
-        let valid = false;
-
-        const min = 3,
-            max = 25;
-
-        const username = usernameEl.value.trim();
-
-        if (!isRequired(username)) {
-            showError(usernameEl, 'Seu usuário não pode estar vazio.');
-        } else if (!isBetween(username.length, min, max)) {
-            showError(usernameEl, `Seu usurário deve ter pelo menos ${min} e ${max} caractéres.`)
-        } else {
-            showSuccess(usernameEl);
-            valid = true;
-        }
-        return valid;
-    };
+//     const form = document.querySelector('#signup');
 
 
-    const checkPassword = () => {
-        let valid = false;
+//     const checkUsername = () => {
+
+//         let valid = false;
+
+//         const min = 3,
+//             max = 25;
+
+//         const username = usernameEl.value.trim();
+
+//         if (!isRequired(username)) {
+//             showError(usernameEl, 'Seu usuário não pode estar vazio.');
+//         } else if (!isBetween(username.length, min, max)) {
+//             showError(usernameEl, `Seu usurário deve ter pelo menos ${min} e ${max} caractéres.`)
+//         } else {
+//             showSuccess(usernameEl);
+//             valid = true;
+//         }
+//         return valid;
+//     };
 
 
-        const password = passwordEl.value.trim();
-
-        if (!isRequired(password)) {
-            showError(passwordEl, 'Sua senha não pode estar vazia.');
-        } else if (!isPasswordSecure(password)) {
-            showError(passwordEl, 'Sua senha deve ter pelo menos 8 caracteres que incluem pelo menos 1 letra minúscula, 1 letra maiúscula, 1 número e 1 caractére especial (!@#$%^&*)');
-        } else {
-            showSuccess(passwordEl);
-            valid = true;
-        }
-
-        return valid;
-    };
+//     const checkPassword = () => {
+//         let valid = false;
 
 
-    const isPasswordSecure = (password) => {
-        const re = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})");
-        return re.test(password);
-    };
+//         const password = passwordEl.value.trim();
 
-    const isRequired = value => value === '' ? false : true;
-    const isBetween = (length, min, max) => length < min || length > max ? false : true;
+//         if (!isRequired(password)) {
+//             showError(passwordEl, 'Sua senha não pode estar vazia.');
+//         } else if (!isPasswordSecure(password)) {
+//             showError(passwordEl, 'Sua senha deve ter pelo menos 8 caracteres que incluem pelo menos 1 letra minúscula, 1 letra maiúscula, 1 número e 1 caractére especial (!@#$%^&*)');
+//         } else {
+//             showSuccess(passwordEl);
+//             valid = true;
+//         }
 
-
-    const showError = (input, message) => {
-        // get the form-field element
-        const formField = input.parentElement;
-        // add the error class
-        formField.classList.remove('success');
-        formField.classList.add('error');
-
-        // show the error message
-        const error = formField.querySelector('small');
-        error.textContent = message;
-    };
-
-    const showSuccess = (input) => {
-        // get the form-field element
-        const formField = input.parentElement;
-
-        // remove the error class
-        formField.classList.remove('error');
-        formField.classList.add('success');
-
-        // hide the error message
-        const error = formField.querySelector('small');
-        error.textContent = '';
-    }
+//         return valid;
+//     };
 
 
-    form.addEventListener('submit', function (e) {
-        // prevent the form from submitting
-        e.preventDefault();
+//     const isPasswordSecure = (password) => {
+//         const re = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})");
+//         return re.test(password);
+//     };
 
-        // validate fields
-        let isUsernameValid = checkUsername(),
-            isEmailValid = checkEmail(),
-            isPasswordValid = checkPassword(),
-            isConfirmPasswordValid = checkConfirmPassword();
-
-        let isFormValid = isUsernameValid &&
-            isEmailValid &&
-            isPasswordValid &&
-            isConfirmPasswordValid;
-
-        // submit to the server if the form is valid
-        if (isFormValid) {
-
-        }
-    });
+//     const isRequired = value => value === '' ? false : true;
+//     const isBetween = (length, min, max) => length < min || length > max ? false : true;
 
 
-    const debounce = (fn, delay = 500) => {
-        let timeoutId;
-        return (...args) => {
-            // cancel the previous timer
-            if (timeoutId) {
-                clearTimeout(timeoutId);
-            }
-            // setup a new timer
-            timeoutId = setTimeout(() => {
-                fn.apply(null, args)
-            }, delay);
-        };
-    };
+//     const showError = (input, message) => {
+//         // get the form-field element
+//         const formField = input.parentElement;
+//         // add the error class
+//         formField.classList.remove('success');
+//         formField.classList.add('error');
 
-    form.addEventListener('input', debounce(function (e) {
-        switch (e.target.id) {
-            case 'username':
-                checkUsername();
-                break;
-            case 'email':
-                checkEmail();
-                break;
-            case 'password':
-                checkPassword();
-                break;
-            case 'confirm-password':
-                checkConfirmPassword();
-                break;
-        }
-    }));
+//         // show the error message
+//         const error = formField.querySelector('small');
+//         error.textContent = message;
+//     };
+
+//     const showSuccess = (input) => {
+//         // get the form-field element
+//         const formField = input.parentElement;
+
+//         // remove the error class
+//         formField.classList.remove('error');
+//         formField.classList.add('success');
+
+//         // hide the error message
+//         const error = formField.querySelector('small');
+//         error.textContent = '';
+//     }
+
+
+//     form.addEventListener('submit', function (e) {
+//         // prevent the form from submitting
+//         e.preventDefault();
+
+//         // validate fields
+//         let isUsernameValid = checkUsername(),
+//             isEmailValid = checkEmail(),
+//             isPasswordValid = checkPassword(),
+//             isConfirmPasswordValid = checkConfirmPassword();
+
+//         let isFormValid = isUsernameValid &&
+//             isEmailValid &&
+//             isPasswordValid &&
+//             isConfirmPasswordValid;
+
+//         // submit to the server if the form is valid
+//         if (isFormValid) {
+//             validacoes()
+//         }
+//     });
+
+
+//     const debounce = (fn, delay = 500) => {
+//         let timeoutId;
+//         return (...args) => {
+//             // cancel the previous timer
+//             if (timeoutId) {
+//                 clearTimeout(timeoutId);
+//             }
+//             // setup a new timer
+//             timeoutId = setTimeout(() => {
+//                 fn.apply(null, args)
+//             }, delay);
+//         };
+//     };
+
+//     form.addEventListener('input', debounce(function (e) {
+//         switch (e.target.id) {
+//             case 'username':
+//                 checkUsername();
+//                 break;
+//             case 'email':
+//                 checkEmail();
+//                 break;
+//             case 'password':
+//                 checkPassword();
+//                 break;
+//             case 'confirm-password':
+//                 checkConfirmPassword();
+//                 break;
+//         }
+//     }));
 // }
--- desafio2-js/js/validacoes.js
+++ desafio2-js/js/validacoes.js
@@ -1,25 +1,24 @@
-export function validacoes(){
+window.onload = function () {
     const username = document.querySelector('#username')
     const password = document.querySelector('#password')
     const login = document.querySelector('#login')
     let validacao = false
     let users = [
-        {"user": "gustavo","senha": "Mart1ns@!@"}
+        { "user": "gustavo", "senha": "123" }
     ]
 
-    login.addEventListener('click', function(){
-
+    login.addEventListener('click', function() {
         let userInput = username.value
         let passwordInput = password.value
 
         validacao = false
-        for(let item of users){
-            if(item["user"] == userInput.trim() && item["senha"] == passwordInput.trim()){
+        for (let item of users) {
+            if (item["user"] == userInput.trim() && item["senha"] == passwordInput.trim()) {
                 validacao = true
-                window.location.href = "./panel.html"
+                window.location.href = './panel.html'
             }
         }
-        if(validacao === false){
+        if (validacao === false) {
             alert("Usuário ou senha errados")
         }
     })
